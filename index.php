73b5ef7f6c49efc88de3a329b03bac449a949f74
Aya Majdoub <ayamajdoub8@gmail.com>
2025-07-06 21:00:07 +0100

    Initial layout of the workshops catalogs

--- index.php
+++ index.php
@@ -3,19 +3,166 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Catalog</title>
 
     <!-- bootstrap css -->
     <link rel = "stylesheet" href = "assets/CSS/bootstrap.min.css">
     <!-- css -->
-    <link rel = "stylesheet" href = "assets/CSS/style.css">
+    <link rel = "stylesheet" href = "assets/CSS/mystyles.css">
+
 </head>
 <body>
     
+    <h1>Our workshops</h1>
+
+    <div class = "container mycontainer">
+        <img src="assets/images/Ashley Buzzy.jfif" width = "125px">
+        
+        <div id = "desc" class = "row">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum velit maxime at voluptate tempora dignissimos neque nihil explicabo labore aliquam eius, magnam quae odit, voluptatibus necessitatibus deserunt reprehenderit ipsum aspernatur.</p>
+        </div>
+    </div>
+    
+    <div id = "c1" class = "carousel slide">
+        <div class="carousel-inner">
+
+            <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class = "card" style="width: 18rem";>
+                            <img class = "card-img-top" src = "assets/images/75d9c6b62d4d928cb1670b91cb146e91.jpg" alt="Theater">
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Theater</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem";>
+
+                        <img class = "card-img-top" src = "assets/images/musicwrkshp.jpg" alt="Music">
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Music</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class = "card" style="width: 18rem";>
+                            <img class = "card-img-top" src = "assets/images/75d9c6b62d4d928cb1670b91cb146e91.jpg" alt="Theater" style="width: 18rem";>
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Theater</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem";>
+
+                        <img class = "card-img-top" src = "assets/images/musicwrkshp.jpg" alt="Music" style="width: 18rem";>
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Music</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class = "card" style="width: 18rem";>
+                            <img class = "card-img-top" src = "assets/images/paintingwrkshp.jpg" alt="Painting">
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Painting</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem";>
+                            <img class = "card-img-top" src = "assets/images/musicwrkshp.jpg" alt="Music">
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Music</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class = "card" style="width: 18rem";>
+                            <img class = "card-img-top" src = "assets/images/75d9c6b62d4d928cb1670b91cb146e91.jpg" alt="Theater" style="width: 18rem";>
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Theater</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem";>
+
+                        <img class = "card-img-top" src = "assets/images/musicwrkshp.jpg" alt="Music" style="width: 18rem";>
+
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Music</h5>
+                                <p class="card-text text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet non quasi officiis? Architecto accusantium sequi, magnam magni soluta maxime veniam, odio quam obcaecati asperiores minima necessitatibus vitae natus a.</p>
+
+                                <a href="" class="btn btn-danger">Register</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                    
+            </div>
+            
+
+        </div>
+
+       <button class="carousel-control-prev" type="button" data-bs-target="#c1" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#c1" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+            <span class="visually-hidden">Next</span>
+        </button> 
+
+    </div>
+    
     <!-- bootstrap js -->
-    <script src = "assets/JavaScript/bootstrap/bundle.min.js"></script>
+    <script src = "assets/JavaScript/bootstrap.bundle.min.js"></script>
     <!-- javascript -->
     <script src = "assets/JavaScript/main.js"></script>
+    
+
 </body>
 </html>
 
